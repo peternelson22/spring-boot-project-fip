774aafd36b5a9a68dca1a3812302b69fb21f0a27
PeteNelson22 <peternelson063@gmail.com>
2022-11-05 13:52:23 +0100

    initial migration

---
+++ src/main/resources/db/migration/V1_0__initial_migration.sql
@@ -0,0 +1,16 @@
+CREATE SEQUENCE  IF NOT EXISTS employee_sequence START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE employees (
+  id BIGINT NOT NULL,
+   first_name VARCHAR(255) NOT NULL,
+   last_name VARCHAR(255),
+   email VARCHAR(255) NOT NULL,
+   gender VARCHAR(255),
+   dob date,
+   employed_date date NOT NULL,
+   sacked_date date,
+   dept_id BIGINT,
+   CONSTRAINT pk_employees PRIMARY KEY (id)
+);
+
+ALTER TABLE employees ADD CONSTRAINT email_unique UNIQUE (email);
